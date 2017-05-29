9a8b685715482fbab8ff54e75016671ab5f89924
yad <yad@yad.com>
2017-05-29 09:47:18 +0200

    feat: fix exception handling and trace

--- ComicBoxApi/ComicBoxApi/Startup.cs
+++ ComicBoxApi/ComicBoxApi/Startup.cs
@@ -40,11 +40,14 @@ namespace ComicBoxApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            //loggerFactory.AddDebug();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            
+            app.UseDeveloperExceptionPage();
 
             app.UseMvc();
-            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
         }
     }
 }
