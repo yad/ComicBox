cff1d80f7adc6f58a1be1c7f8e1837c30e44ddaa
yad <yad@yad.com>
2017-06-02 19:47:56 +0200

    feat: provide support for port binding from command line

--- ComicBoxApi/ComicBoxApi/Startup.cs
+++ ComicBoxApi/ComicBoxApi/Startup.cs
@@ -3,27 +3,73 @@ using ComicBoxApi.App.Cache;
 using ComicBoxApi.App.FileBrowser;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace ComicBoxApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public static void Main(string[] args)
         {
-            Configuration = configuration;
+            CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
         }
 
-        public IConfiguration Configuration { get; }
+        public static IWebHostBuilder CreateDefaultBuilder(string[] args)
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureAppConfiguration(BuildAppConfiguration(args))
+                .UseConfiguration(BuildConfiguration(args))
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureLogging(BuildLogging());
+
+            return builder;
+        }
+
+        private static Action<WebHostBuilderContext, IConfigurationBuilder> BuildAppConfiguration(string[] args)
+        {
+            return (hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.SetBasePath(env.ContentRootPath);
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                config.AddEnvironmentVariables();
+                config.AddCommandLine(args);
+            };
+        }
+
+        private static IConfiguration BuildConfiguration(string[] args)
+        {
+            // Hack, check how dotnet team handle this in dotnet 2.0 preview
+            return new ConfigurationBuilder().AddCommandLine(args).Build();
+        }
+
+        private static Action<WebHostBuilderContext, LoggerFactory> BuildLogging()
+        {
+            return (hostingContext, logging) =>
+            {
+                logging.UseConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+            };
+        }
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private readonly IConfiguration _configuration;
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -32,7 +78,7 @@ namespace ComicBoxApi
             // Add framework services.                
             services.AddMemoryCache();
 
-            var externalFileProvider = new PhysicalFileProvider(Configuration["Settings:AbsoluteBasePath"]);
+            var externalFileProvider = new PhysicalFileProvider(_configuration["Settings:AbsoluteBasePath"]);
             var compositeProvider = new CompositeFileProvider(externalFileProvider);
             services.AddSingleton<IFileProvider>(compositeProvider);
 
@@ -51,53 +97,6 @@ namespace ComicBoxApi
             app.UseStaticFiles();
 
             app.UseMvc();
-        }
-
-        public static IWebHostBuilder CreateDefaultBuilder(string[] args)
-        {
-            var builder = new WebHostBuilder()
-                .ConfigureAppConfiguration(BuildAppConfiguration(args))
-                .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
-                .UseKestrel()
-                .UseIISIntegration()
-                .UseContentRoot(Directory.GetCurrentDirectory())                
-                .ConfigureLogging(BuildLogging());
-                
-                //.UseDefaultServiceProvider((context, options) => {})
-                //.ConfigureServices(BuildServices());
-
-            return builder;
-        }
-
-        private static Action<WebHostBuilderContext, IConfigurationBuilder> BuildAppConfiguration(string[] args)
-        {
-            return (hostingContext, config) =>
-            {
-                var env = hostingContext.HostingEnvironment;                
-                config.SetBasePath(env.ContentRootPath);
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-                config.AddEnvironmentVariables();
-                config.AddCommandLine(args);
-            };
-        }
-
-        private static Action<WebHostBuilderContext, LoggerFactory> BuildLogging()
-        {
-            return (hostingContext, logging) =>
-            {
-                logging.UseConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddConsole();
-                logging.AddDebug();
-            };
-        }
-
-        private static Action<IServiceCollection> BuildServices()
-        {
-            return services =>
-            {
-                //services.AddTransient<IConfigureOptions<KestrelServerOptions>, KestrelServerOptionsSetup>();
-            };
         }
     }
 }
