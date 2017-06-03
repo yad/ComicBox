1b46481d4e28b4ef8ff3317b19be360aabf9c155
yad <yad@yad.com>
2017-06-03 18:41:38 +0200

    no message

--- full-build.bat
+++ full-build.bat
@@ -1,11 +1,11 @@
 @echo off
 cd ComicBoxApi\ComicBoxApi
-REM dotnet restore
+dotnet restore
 dotnet publish --configuration Release --output .\..\..\Build\
 cd ..\..
-REM CALL npm install -g @angular/cli
+CALL npm install -g @angular/cli
 cd ComicBoxDisplay\comic-box-display
-REM CALL npm install
+CALL npm install
 CALL npm run ng build --prod
 md .\..\..\Build\wwwroot\
 move /Y dist\* .\..\..\Build\wwwroot\
